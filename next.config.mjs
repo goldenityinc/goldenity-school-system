fd14b99243dc7a3b669e7b2a08681b67d433835e
School Sistem Pertama <goldenityinc@gmail.com>
2026-07-16 21:34:38 +0700

    chore: bypass ts checks during next build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  typescript: {
+    // Dangerously allow production builds even when type errors exist.
+    ignoreBuildErrors: true
+  }
 };
 
 export default nextConfig;
